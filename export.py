3e9ff58dfdfb9198eaae04386385ee703e5932dc
spirgiuz <flymon@gmail.com>
2019-05-12 00:19:45 +0300

    export.py fix html character escaping

--- export.py
+++ export.py
@@ -2,16 +2,18 @@
 import requests
 import json
 import re
+import urllib
 
 client_id="170237838334492682" #seems to be the blarg.xyz client id
 serverid=""
-
+blargauthdone=False
 session = requests.Session()
 
 
 def blargauth():
 	global serverid
 	global session
+	print("doing blargauth")
 	f=open("credentials.txt")
 	login=f.readline().strip()
 	password=f.readline().strip()
@@ -32,11 +34,20 @@ def blargauth():
 	session.get(callbackurl)
 
 def save(tagname,content,session,serverid):
+	global blargauthdone
+	if not blargauthdone:
+		blargauth()
+		blargauthdone=True
 	headersblarg={"Content-Type":"application/x-www-form-urlencoded"}
-	payload3="tagName={}&newname=&action={}&destination={}&fontsize=20&content={}".format(tagname,"save",serverid,content).encode("utf-8")
-	session.post('https://blargbot.xyz/tags/editor',data=payload3,headers=headersblarg)
+	payload3="tagName={}&newname=&action={}&destination={}&fontsize=20&content={}".format(tagname,"save",serverid,urllib.parse.quote(content)).encode("utf-8")
+	r=session.post('https://blargbot.xyz/tags/editor',data=payload3,headers=headersblarg)
+	print("save response {}".format(r.status_code))
 
 def load(tagname,session,serverid):
+	global blargauthdone
+	if not blargauthdone:
+		blargauth()
+		blargauthdone=True
 	headersblarg={"Content-Type":"application/x-www-form-urlencoded"}
 	payload3="tagName={}&newname=&action={}&destination={}&fontsize=20".format(tagname,"load",serverid)
 	r= session.post('https://blargbot.xyz/tags/editor',data=payload3,headers=headersblarg)
@@ -45,22 +56,25 @@ def load(tagname,session,serverid):
 	f=open("{}.bbtag".format(tagname),"w")
 	f.write(result.group(1).replace("\\r\\n","\n"))
 
-def gitsave(giturl,session,serverid):
+def gitsave(giturl,session,serverid,tagname=[]):
 	r=requests.get(giturl)
 	gitjson=json.loads(r.text)
 	for tag in gitjson:
 		if "bbtag" in tag["name"]:
 			name=tag["name"].split(".")[0]
-			print("saving {}".format(name))
-			r2=requests.get(tag["download_url"])
-			save(name,r2.text,session,serverid)
+			if len(tagname)==0 or name in tagname:
+				print("saving {}".format(name))
+				r2=requests.get(tag["download_url"])
+				save(name,r2.text,session,serverid)
 
 def backup(list,session,serverid):
 	for item in list:
 		load(item,session,serverid)
 
-blargauth()
+
+
 #load("getfromgit",session,serverid)
 #save("getfromgit2","test321",session,serverid)
-#gitsave("https://api.github.com/repos/spirgiuz/bso_discord/contents/blargbot",session,serverid)
-backup(["afk","jordan","joined"],session,serverid)
+gitsave("https://api.github.com/repos/spirgiuz/bso_discord/contents/blargbot",session,serverid,["adjustrs"])
+#backup(["afk","jordan","joined"],session,serverid)
+
